5ff796e060469335dc125934a441912941d39c53
Paul Ambrose <pambrose@mac.com>
2019-03-27 12:16:20 -0700

    Redo packages

--- src/robot_controller_server.py
+++ src/robot_controller_server.py
@@ -10,9 +10,8 @@ from flask import Response
 from flask import jsonify
 from flask_httpauth import HTTPBasicAuth
 from geometry_msgs.msg import Twist
-
-from src.utils import new_twist
-from src.utils import setup_logging
+from utils import new_twist
+from utils import setup_logging
 
 PORT = 'port'
 LOG_LEVEL = 'loglevel'
