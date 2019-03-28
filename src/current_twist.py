4ca6cb2cb6ea40f9c0316e1a674d584001a0a7a6
Paul Ambrose <pambrose@mac.com>
2019-03-27 22:51:10 -0700

    Add debugging info

--- src/current_twist.py
+++ src/current_twist.py
@@ -32,23 +32,23 @@ class CurrentTwist(object):
             self._angular = val
 
     def forward(self):
-        self._linear = min(self._linear + .1, .5)
+        self._linear = min(self._linear + 0.1, 0.5)
 
     def backward(self):
-        self._linear = max(self._linear - .1, -.5)
+        self._linear = max(self._linear - 0.1, -0.5)
 
     def left(self):
-        self._angular = min(self._angular + .1, .5)
+        self._angular = min(self._angular + 0.1, 0.5)
 
     def right(self):
-        self._angular = max(self._angular - .1, -.5)
+        self._angular = max(self._angular - 0.1, -0.5)
 
     def stop(self):
-        self._linear = 0
-        self._angular = 0
+        self._linear = 0.0
+        self._angular = 0.0
 
     def twist_msg(self):
         return new_twist(self.linear, self.angular)
 
     def json(self):
-        return jsonify({'linear': round(self.linear, 1), 'angular': round(self.angular, 2)})
+        return jsonify({'linear': round(self.linear, 1), 'angular': round(self.angular, 1)})
